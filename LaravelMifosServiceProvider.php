b9696cf13a86c000199249f92b9ab1222a1d80e4
le-yo <le-yo@leyo.com>
2022-03-16 00:20:49 +0300

    Added service provider

---
+++ LaravelMifosServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Helaplus\LaravelMifos;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelMifosServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
