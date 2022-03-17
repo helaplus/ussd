287159591158cb3914b457aaca9d3168c28a315c
le-yo <le-yo@leyo.com>
2022-03-17 22:28:12 +0300

    initial ussd package

---
+++ Database/Seeders/UssdDatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modules\Ussd\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class UssdDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+
+        // $this->call("OthersTableSeeder");
+    }
+}
