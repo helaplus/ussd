20cdccff1002875ebb8425140c5095e85723e9db
le-yo <le-yo@leyo.com>
2022-03-18 02:12:23 +0300

    Adding route prefix configuration

--- src/UssdServiceProvider.php
+++ src/UssdServiceProvider.php
@@ -23,15 +23,15 @@ class UssdServiceProvider extends ServiceProvider
                     InstallUssdPackage::class,
                 ]);
         }
-//        Route::group($this->routeConfiguration(), function () {
+        Route::group($this->routeConfiguration(), function () {
             $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
-//        });
-    }
+        });
+    } 
 
     protected function routeConfiguration()
     {
         return [
-            'prefix' => config('ussd.prefix')
+            'prefix' => config('prefix')
         ];
     }
 }
