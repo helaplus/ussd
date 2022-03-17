a742467ce9adcf8fb4ba7e2089fd1e26ac2acf40
le-yo <le-yo@leyo.com>
2022-03-18 02:06:45 +0300

    Fixing route loading location

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-//use Helaplus\Ussd\Http\Controllers\UssdController;
+use Helaplus\Ussd\Http\Controllers\UssdController;
 //
-//Route::get('/app', [UssdController::class, 'index'])->name('ussd.index');
+Route::get('/app', [UssdController::class, 'index'])->name('ussd.index');
