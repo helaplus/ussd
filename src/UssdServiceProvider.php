37277b1241b029467e0f16d3de98904640f060fb
le-yo <le-yo@leyo.com>
2022-03-18 02:03:49 +0300

    Added route prefix

--- src/UssdServiceProvider.php
+++ src/UssdServiceProvider.php
@@ -23,15 +23,15 @@ class UssdServiceProvider extends ServiceProvider
                     InstallUssdPackage::class,
                 ]);
         }
-//        Route::group($this->routeConfiguration(), function () {
-//            $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
-//        });
+        Route::group($this->routeConfiguration(), function () {
+            $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        });
     }
 
-//    protected function routeConfiguration()
-//    {
-//        return [
-//            'prefix' => config('ussd.prefix')
-//        ];
-//    }
+    protected function routeConfiguration()
+    {
+        return [
+            'prefix' => config('ussd.prefix')
+        ];
+    }
 }
