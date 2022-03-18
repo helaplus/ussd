1472a176e6814f5d33ade780963f0630b21a7f3d
le-yo <le-yo@leyo.com>
2022-03-18 03:30:41 +0300

    Added ussd log model

---
+++ src/Models/UssdLog.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Helaplus\Ussd\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UssdLog extends Model
+{
+    use HasFactory;
+    protected $fillable = [];
+    protected $guarded = [];
+}
