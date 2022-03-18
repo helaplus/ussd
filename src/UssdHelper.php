444824b569656b2c1ef054532b478b5d4b003cdd
le-yo <le-yo@leyo.com>
2022-03-18 06:47:38 +0300

    Added continueUssdProgress

--- src/UssdHelper.php
+++ src/UssdHelper.php
@@ -244,11 +244,263 @@ class UssdHelper {
         exit;
     }
 
-    public static function replaceTemplates($session,$response){
+    public static function replaceTemplates($state,$response){
         //$search  = array('{param}','{param2}'); // sample
         //$replace = array('{param}','{param2');
         //$response = str_replace($search, $replace, $response);
         return $response;
+    }
+
+
+
+    public static function continueUssdProgress($state, $message)
+    {
+        $response = '';
+        $menu = UssdMenu::find($state->menu_id);
+        //check the user menu
+
+        switch ($menu->type) {
+            case 0:
+                //authentication mini app
+                break;
+            case 1:
+                //continue to another menu
+                $response = self::continueUssdMenu($state, $message, $menu);
+                break;
+            case 2:
+
+                //continue to a processs
+                $response = self::continueSingleProcess($state, $message, $menu);
+                break;
+            case 3:
+                //continue to a processs
+                $response = self::continueSingleProcess($state, $message, $menu);
+                break;
+            case 4:
+
+                $response = self::customApp($state, $menu, $message);
+
+                break;
+            default :
+                self::resetUser($state);
+                $response = "An authentication error occurred";
+                break;
+        }
+
+        return $response;
+
+    }
+    
+    public static function continueSingleProcess($state, $message, $menu)
+    {
+        $response = "";
+        self::storeUssdResponse($state, $message);
+
+        //validate input to be numeric
+        $menuItem = UssdMenuItems::whereMenuIdAndStep($menu->id, $state->progress)->first();
+        if($menuItem->validation == 'custom'){
+            if(self::customValidation($state,$message,$menuItem)){
+                $step = $state->progress + 1;
+            }
+        }elseif(strpos($menuItem->validation, '_preset')) {
+
+            if(self::presetValidation($state,$message,$menuItem)){
+                $step = $state->progress + 1;
+            }
+        }elseif($menuItem->validation == 'schedule'){
+
+        }else{
+                //validation is fine
+                $step = $state->progress + 1;
+        }
+
+        $menuItem = UssdMenuItems::whereMenuIdAndStep($menu->id, $step)->first();
+
+        if ($menuItem) {
+            $state->menu_item_id = $menuItem->id;
+            $state->progress = $step;
+            $state->save();
+            return $response. $menuItem->description;
+        } else {
+                $response = self::confirmBatch($state, $menu);
+            return $response;
+        }
+    }
+    public static function presetValidation($state,$message,$menuItem){
+        switch ($menuItem->validation) {
+            case "nationalId_preset":
+
+                $config = MifosUssdConfig::whereAppId($state->app_id)->first();
+
+                //validate national ID from Mifos
+                $response = MifosHelperController::getClientByNationalId($message,$config);
+
+                if(isset($response[0])){
+                    if($response[0]->entityType == 'CLIENTIDENTIFIER'){
+                        //check if ID belongs to the same client
+                        $client = MifosHelperController::getClientbyClientId($response[0]->parentId,$config);
+                        if(substr($client->mobileNo,-9) == (substr($state->phone,-9))){
+                            $client_details = array('client_id'=>$response[0]->parentId,'external_id'=>$message);
+                            $state->other = json_encode($client_details);
+                            $state->save();
+                            return TRUE;
+                        }else{
+                            $response = "National ID is valid but belongs to a different phone number.".PHP_EOL."Please enter your ID";
+                            self::sendResponse($response,1,$state);
+                        }
+                    }elseif($response[0]->entityType == 'CLIENT'){
+                        //check if ID belongs to the same client
+                        $client = MifosHelperController::getClientbyClientId($response[0]->entityId,$config);
+
+                        if(substr($client->mobileNo,-9) == (substr($state->phone,-9))){
+                            $client_details = array('client_id'=>$response[0]->entityId,'external_id'=>$message);
+                            $state->other = json_encode($client_details);
+                            $state->save();
+                            return TRUE;
+                        }else{
+                            $response = "National ID is valid but belongs to a different phone number.".PHP_EOL."Please enter your ID";
+                            self::sendResponse($response,1,$state);
+                        }
+                    }{
+                        return FALSE;
+                    }
+                }else{
+                    $response = "National ID is not registered. Service only available to registered customers";
+                    self::sendResponse($response,1,$state);
+                }
+                break;
+            case "confirm_pin_preset":
+
+                //veify if the PINs are equal
+                $PIN = UssdResponse::wherePhoneAndMenuIdAndMenuItemId($state->phone, $state->menu_id,54)->orderBy('id', 'DESC')->first();
+                $CONFIRM_PIN = UssdResponse::wherePhoneAndMenuIdAndMenuItemId($state->phone, $state->menu_id,55)->orderBy('id', 'DESC')->first();
+//                print_r($PIN->response);
+//                exit;
+                if($PIN->response == $CONFIRM_PIN->response){
+                    //set PIN and send to Mifos
+                    $datatable = array(
+                        "PIN" => Crypt::encrypt($PIN->response),
+                        "locale"=>"en",
+                        "dateFormat"=> "dd MMMM yyyy"
+                    );
+                    $config = MifosUssdConfig::whereAppId($state->app_id)->first();
+                    $client_details = json_decode($state->other);
+                    $client_details->pin = Crypt::encrypt($PIN->response);
+                    $r = MifosHelperController::setDatatable('PIN',$client_details->client_id,json_encode($datatable),$config);
+                    if (!empty($r->errors)) {
+
+                        if (strpos($r->defaultUserMessage, 'already exists')) {
+                            //we try to update
+                            $r = MifosHelperController::updateDatatable('PIN',$client_details->client_id,json_encode($datatable),$config,1);
+                        }
+                        if(!empty($r->errors)){
+                            $error_msg = 'We had a problem setting your PIN. Kindly retry or contact Customer Care';
+                            self::sendResponse($error_msg,1,$state);
+                        }
+                    }
+                    // post the encoded application details
+//                    $r = MifosHelperController::MifosPostTransaction($postURl, json_encode($datatable),$config);
+                    //store PIN in session
+                    $client_details->pin = Crypt::encrypt($PIN->response);
+                    $state->other = json_encode($client_details);
+                    $state->save();
+                    return TRUE;
+                }else{
+                    $step = $state->progress - 1;
+                    $state->progress = $step;
+                    $state->save();
+                    return FALSE;
+                }
+                break;
+            case "auth_pin_preset":
+
+                if($message == '0' && strlen($message)==1){
+                    $menu = UssdMenu::find(12);
+                    $response = MifosUssdHelperController::nextMenuSwitch($state,$menu);
+                    MifosUssdHelperController::sendResponse($response, 1, $state,null);
+                }else{
+                    $response = self::validatePIN($state,$message);
+                }
+                break;
+            case "auth_nationalId_preset":
+                $config = MifosUssdConfig::whereAppId($state->app_id)->first();
+                //validate national ID from Mifos
+                $response = MifosHelperController::getClientByNationalId($message,$config);
+                if(isset($response[0])){
+                    if($response[0]->entityType == 'CLIENTIDENTIFIER'){
+                        //check if ID belongs to the same client
+                        $client = MifosHelperController::getClientbyClientId($response[0]->parentId,$config);
+                        if(substr($client->mobileNo,-9) == (substr($state->phone,-9))){
+                            $client_details = array('client_id'=>$response[0]->parentId,'external_id'=>$message);
+                            $state->other = json_encode($client_details);
+                            return TRUE;
+                        }else{
+                            $response = "National ID is valid but belongs to a different phone number.".PHP_EOL."Please enter your ID";
+                            self::sendResponse($response,1,$state);
+                        }
+                    }else{
+                        return FALSE;
+                    }
+                }
+                break;
+            case "auth2_nationalId_preset":
+                //veify if the IDs are equal
+                $PIN = UssdResponse::wherePhoneAndMenuIdAndMenuItemId($state->phone, $state->menu_id,2)->orderBy('id', 'DESC')->first();
+                $CONFIRM_PIN = UssdResponse::wherePhoneAndMenuIdAndMenuItemId($state->phone, $state->menu_id,2)->orderBy('id', 'DESC')->first();
+                if($PIN->response == $CONFIRM_PIN->response){
+                    //set PIN and send to Mifos
+                    $datatable = array(
+                        "PIN" => Crypt::encrypt($PIN->response),
+                        "locale"=>"en",
+                        "dateFormat"=> "dd MMMM yyyy"
+                    );
+                    $config = MifosUssdConfig::whereAppId($state->app_id)->first();
+                    $client_details = json_decode($state->other);
+                    $r = MifosHelperController::setDatatable('PIN',$client_details->client_id,json_encode($datatable),$config);
+
+                    if (!empty($r->errors)) {
+
+                        if (strpos($r->defaultUserMessage, 'already exists')) {
+                            //we try to update
+                            $r = MifosHelperController::updateDatatable('PIN',$client_details->client_id,json_encode($datatable),$config);
+                        }
+                        if(!empty($r->errors)){
+                            $error_msg = 'We had a problem setting your PIN. Kindly retry or contact Customer Care';
+                            self::sendResponse($error_msg,1,$state);
+                        }
+                    }
+                    // post the encoded application details
+//                    $r = MifosHelperController::MifosPostTransaction($postURl, json_encode($datatable),$config);
+
+                    //store PIN in session
+//                    print_r($PIN->response);
+//                    exit;
+                    $client_details->pin = Crypt::encrypt($PIN->response);
+                    $state->other = json_encode($client_details);
+                    $state->save();
+
+                    return TRUE;
+                }else{
+                    $step = $state->progress - 1;
+                    $state->progress = $step;
+                    $state->save();
+                    return FALSE;
+                }
+                break;
+
+            default :
+                break;
+        }
+        return $response;
+    }
+
+    public static function customValidation($state,$message,$menuItem){
+        switch ($menuItem->id) {
+            default :
+                return TRUE;
+                break;
+        }
+
     }
 
 }
