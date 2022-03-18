76c9e982c5496dc75cbca06f89e56a35ad0c4c9e
le-yo <le-yo@leyo.com>
2022-03-18 05:28:37 +0300

    Default configuration for welcome and error messages

--- config/config.php
+++ config/config.php
@@ -1,5 +1,13 @@
 <?php
 
 return [
+
+    //route prefix
     'prefix' => 'ussd',
+
+    //default welcome message
+    'default_welcome_message' => 'Welcome to '.env('APP_NAME'),
+
+    //default welcome message
+    'default_error_message' => 'We experienced an error processing your request',
 ];
