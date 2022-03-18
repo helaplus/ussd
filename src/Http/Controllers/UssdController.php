2902ea13d5900d48ce4cdb98f02759098eb64a28
le-yo <le-yo@leyo.com>
2022-03-18 05:28:16 +0300

    Added logic to power the ussd app

--- src/Http/Controllers/UssdController.php
+++ src/Http/Controllers/UssdController.php
@@ -4,6 +4,7 @@
 namespace Helaplus\Ussd\Http\Controllers;
 
 
+use Helaplus\Ussd\Models\UssdMenu;
 use Helaplus\Ussd\Models\UssdState;
 use Helaplus\Ussd\Ussd;
 use Helaplus\Ussd\UssdHelper;
@@ -12,19 +13,20 @@ use Illuminate\Http\Request;
 class UssdController extends Controller
 {
 
-    public function __construct()
-    {
-
-    }
 
     public function app(Request $request){
+
         //get Inputs
         $input = UssdHelper::getInputs($request);
 
-        //get phone user ussd state
+        //using the phone number get the ussd state
         $state = UssdState::firstorcreate([ 'phone'=>$input->phone]);
 
-        return "CON Welcome to ".env('APP_NAME');
+        //route request
+        $response = UssdHelper::isUserStarting() ? UssdHelper::getHomeMenu($state) : UssdHelper::stateSwitch($input,$state);
+
+        //sendResponse
+        return UssdHelper::sendResponse($response,1,$state,$input);
     }
 
 
