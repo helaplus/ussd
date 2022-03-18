465a3753b5517095505b273ad0f0b1e52fe08ceb
le-yo <le-yo@leyo.com>
2022-03-18 05:27:50 +0300

    Added skiplogic

---
+++ src/Models/UssdUserMenuSkipLogic.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Helaplus\Ussd\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UssdUserMenuSkipLogic extends Model
+{
+    protected $table='ussd_user_menu_skip_logic';
+    protected $fillable = [];
+    protected $guarded = [];
+}
