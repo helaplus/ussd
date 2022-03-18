dc818abde34854a676ecbf9a5df23e632a16dc3c
le-yo <le-yo@leyo.com>
2022-03-18 07:00:48 +0300

    Testing skiplogic

--- src/UssdHelper.php
+++ src/UssdHelper.php
@@ -162,6 +162,8 @@ class UssdHelper {
     public static function checkMenuSkipLogic($state,$menu){
         $skipLogic = UssdUserMenuSkipLogic::wherePhoneAndUssdMenuId($state->phone,$menu->id)->first();
         if($skipLogic){
+            print_r($skipLogic);
+            exit;
         while($skipLogic->skip == 1) {
             $menu = UssdMenu::find($menu->next_ussd_menu_id);
             $skipLogic = UssdUserMenuSkipLogic::wherePhoneAndUssdMenuId($state->phone,$menu->id)->first();
