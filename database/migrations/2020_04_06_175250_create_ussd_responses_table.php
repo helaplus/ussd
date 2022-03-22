84b25a98228e87d2c9c4b6aec59dbecba2a6df55
le-yo <le-yo@leyo.com>
2022-03-22 12:14:25 +0300

    Changed response to  long text

--- database/migrations/2020_04_06_175250_create_ussd_responses_table.php
+++ database/migrations/2020_04_06_175250_create_ussd_responses_table.php
@@ -18,7 +18,7 @@ class CreateUssdResponsesTable extends Migration
             $table->string('phone')->default(0);
             $table->integer('menu_id')->unsigned()->nullable();
             $table->integer('menu_item_id')->unsigned()->nullable();
-            $table->string('response', 45);
+            $table->longText('response', 45);
             $table->timestamps();
         });
     }
