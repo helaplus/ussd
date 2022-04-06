bb42d42c7c18aac710c5cc159dfec03d74126495
le-yo <le-yo@le-yo.com>
2022-04-07 01:25:05 +0300

    Added sample listener

---
+++ src/Listeners/UssdEventListener.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Listeners;
+
+use Carbon\Carbon;
+use Helaplus\Sms\Http\Controllers\SmsController;
+use Helaplus\Ussd\Events\UssdEvent;
+use Illuminate\Support\Facades\Http;
+
+class UssdEventListener
+{
+    public function handle(UssdEvent $event)
+    {
+        self::eventSwitch($event);
+    }
+
+    public function eventSwitch($event){
+
+        switch ($event->eventType) {
+            case 'UssdUserRegistered':
+                //do something
+                self::processRegisteredUser($event)
+                break;
+            case 'sms':
+                self::sendSms($event);
+                break;
+            default:
+                break;
+        }
+    }
+    public static function processRegisteredUser($event){
+
+    }
+    public static function sendSms($event){
+        SmsController::sendSms($event->state->phone,$event->state->sms);
+    }
+
+}
